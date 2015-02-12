0215d5af161de6dec856fdbf43cac7d2a89cd0bb
sungminchang <4sungmin@gmail.com>
2015-02-12 11:34:12 -0800

    Added more comments and removed old code

--- src/solvers.js
+++ src/solvers.js
@@ -38,10 +38,7 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = new Board({n:n}); //fixme
-  // var arrOfBoards = {};
-
   queenHelper(0, solution, false);
-
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
   return solution.rows();
 };
@@ -51,10 +48,8 @@ window.findNQueensSolution = function(n) {
 //========= START countNQueensSolutions ============
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = 0; //fixme
   var board = new Board({n:n});
-
-  solutionCount = queenHelper(0, board, true);
+  var solutionCount = queenHelper(0, board, true);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
@@ -66,10 +61,13 @@ window.countNQueensSolutions = function(n) {
 // numberOfRooksPlaced: # of rooks current placed on board
 // board: A reference to a board object
 // count: A boolean determining whether to stop at first solution or continue
+// This function will find all solutions unless count=false
+// and return the # of solutions possible for n-rooks.
+// This will augment the board passed in by reference
 var rookHelper = function(numberOfRooksPlaced, board, count){
-  // Base Cases
   var numberOfRows = board.get("n");
   var sum = 0;
+
   // If the board has any conflicts, return 0
   if(board.hasAnyRowConflicts() || board.hasAnyColConflicts()){
     return 0;
@@ -82,13 +80,16 @@ var rookHelper = function(numberOfRooksPlaced, board, count){
 
   // Loop through the current row
   for(var i = 0; i < numberOfRows; i++){
-
-    // Current state of board is good, augment a new board
+    // Since the current state of board is good, augment it
     // and pass that recursively
     board.rows()[numberOfRooksPlaced][i] = 1;
     sum += rookHelper(numberOfRooksPlaced+1, board, count);
+
+    // Return if we just want to find a single solution
     if(sum && !count)
       return sum;
+
+    // Remove augmentation
     board.rows()[numberOfRooksPlaced][i] = 0;
   }
   return sum;
@@ -98,37 +99,37 @@ var rookHelper = function(numberOfRooksPlaced, board, count){
 // numberOfQueensPlaced: # of queens current placed on board
 // board: A reference to a board object
 // count: A boolean determining whether to stop at first solution or continue
+// This function will find all solutions unless count=false
+// and return the # of solutions possible for n-queens.
+// This will augment the board passed in by reference
 var queenHelper = function(numberOfQueensPlaced, board, count){
-
-    // Base Cases
-    var numberOfRows = board.get("n");
-    var sum = 0;
-
-    if(board.hasAnyRowConflicts() || board.hasAnyColConflicts()
-    || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()){
-        return 0;
-    }
-
-    if(numberOfRows === numberOfQueensPlaced){
-      // var key = JSON.stringify(board.rows());
-      // if(arrOfBoards[key] !== true){
-      //   // solution = board.rows();
-        return 1;
-      // }
-    }
-
-    // Loop through the current row
-    for(var i = 0; i < numberOfRows; i++){
-      // Current state of board is good, augment a new board
-      // and pass that recursively
-      board.rows()[numberOfQueensPlaced][i] = 1;
-      sum += queenHelper(numberOfQueensPlaced+1, board, count);
-      if (sum && !count)
-        return sum;
-      board.rows()[numberOfQueensPlaced][i] = 0;
-    }
-
-    return sum;
+  var numberOfRows = board.get("n");
+  var sum = 0;
+  // If the board has any conflicts, return 0
+  if(board.hasAnyRowConflicts() || board.hasAnyColConflicts()
+  || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()){
+      return 0;
   }
+
+  // If the # of rows === # of rooks placed, we have found a solution
+  if(numberOfRows === numberOfQueensPlaced){
+      return 1;
+  }
+
+  // Loop through the current row
+  for(var i = 0; i < numberOfRows; i++){
+    // Since the current state of board is good, augment it
+    // and pass that recursively
+    board.rows()[numberOfQueensPlaced][i] = 1;
+    sum += queenHelper(numberOfQueensPlaced+1, board, count);
+
+    // Return if we just want to find a single solution
+    if (sum && !count)
+      return sum;
+
+    board.rows()[numberOfQueensPlaced][i] = 0;
+  }
+  return sum;
+}
 
 //========= End Helper Functions ========
